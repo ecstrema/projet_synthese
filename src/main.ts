231f0cbe8d504ac7530e31ef3b01dde4eb899a2f
Marr11317 <remimarche@gmail.com>
2021-02-27 02:03:34 -0500

    play with mouse when bluetooth not available

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import { BbGame } from "./bb-game";
 import { BbBluetooth } from "./bb-bluetooth";
 
+function startGame() {
+    const htmlExceptGame = document.getElementById("htmlExceptGame");
+    htmlExceptGame?.style.setProperty("visibility", "hidden");
+    const game = BbGame.getGame();
+    game.start();
+}
+
+if (!navigator.bluetooth) {
+    startGame();
+}
+
 const bluetoothButton = document.getElementById("bluetoothButton");
 bluetoothButton?.addEventListener("click", (ev: Event) => {
     BbBluetooth.connect().then(() => {
-        const htmlExceptGame = document.getElementById("htmlExceptGame");
-        htmlExceptGame?.style.setProperty("visibility", "hidden");
-        const game = BbGame.getGame();
-        game.start();
+        startGame();
     }).catch((err) => {
         console.error("An error ocurred:", err);
     })
