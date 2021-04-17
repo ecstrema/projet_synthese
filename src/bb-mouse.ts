bd1842727bc96c6ad89bbd619e65da399b83027b
Marr11317 <remimarche@gmail.com>
2021-04-17 10:59:13 -0400

    undo cleanup BbMouse

--- src/bb-mouse.ts
+++ src/bb-mouse.ts
@@ -1,14 +1,17 @@
 export class BbMouse {
     static init() {
         window.addEventListener("mousemove", (ev: MouseEvent) => {
+            BbMouse.mouseX = ev.clientX;
             BbMouse.mouseY = ev.clientY;
         })
         window.addEventListener("touchmove", (ev: TouchEvent) => {
             ev.preventDefault();
+            BbMouse.mouseX = ev.touches[0].clientX;
             BbMouse.mouseY = ev.touches[0].clientY;
         }, {passive: false})
     }
 
+    static mouseX: number = -1;
     static mouseY: number = -1;
 
 }
